7487dfbd210684089cc9ab702527018ac7724ff2
mgud1989 <m.gud1989@gmail.com>
2020-03-14 18:34:10 -0300

    Se agrega el metodo to_handle, se instancia cost_calculator, y se utiliza el metodo calculate

--- delete_if.py
+++ delete_if.py
@@ -1,17 +1,12 @@
-def call_cost_calculate(call):
-    cost = 0
-    if call.is.local():
-        cost = calculate_local_cost_of(call)
-    elif call.is.national():
-        cost = calculate_national_cost_of(call)
-    elif call.is.international():
-        cost = calculate_international_cost_of(call)
-
-    return cost
 
 class CallCostCalculator(object):
-    def m(self):
+    @classmethod
+    def to_handle(klass, call):
+        #codigo que busca el CallCostCalculator correspondiente
+
+    def calculate(self):
         raise NotImplementedError("Subclass Responsability")
+
 class LocalCallCostCalculator(CallCostCalculator):
     def m(self):
         #codigo de calculate_local_cost_of
@@ -21,3 +16,6 @@ class NationalCallCostCalculator(CallCostCalculator):
 class InternationalCallCostCalculator(CallCostCalculator):
     def m(self):
         #codigo de calculate_international_cost_of
+
+cost_calculator = CallCostCalculator.to_handle(call)
+cost_calculator.calculate()
